1bc35b8479b6563f7229705d5be4bf3c018d5fcf
joy-arz <jony.huang.me@gmail.com>
2026-05-12 12:51:31 +0700

    Further increase logo size

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,7 +2,7 @@
   <div class="max-w-6xl mx-auto px-6 lg:px-8">
     <div class="flex items-center justify-between h-16 md:h-20">
       <a href="/{{ $locale }}" class="flex items-center gap-3 cursor-pointer">
-        <img id="navbar-logo" src="/images/Logo/InaBCRU_LOGO CERAH.webp" alt="InaBCRU" class="h-14 md:h-16 w-auto object-contain transition-opacity duration-300">
+        <img id="navbar-logo" src="/images/Logo/InaBCRU_LOGO CERAH.webp" alt="InaBCRU" class="h-20 md:h-24 w-auto object-contain transition-opacity duration-300">
       </a>
 
       <div class="hidden xl:flex items-center gap-1">
