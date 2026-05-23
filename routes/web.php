79583286f1886641299d69f836b6c371ea89e4df
joy-arz <jony.huang.me@gmail.com>
2026-05-23 11:35:08 +0700

    Fix missing imports for DivisionController and ProgramController

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\Admin\TeamController;
 use App\Http\Controllers\Admin\PartnerController;
 use App\Http\Controllers\Admin\SiteImageController;
 use App\Http\Controllers\Admin\FileUploadController;
+use App\Http\Controllers\Admin\DivisionController;
+use App\Http\Controllers\Admin\ProgramController;
 use App\Models\Publication;
 use App\Models\Article;
 use App\Models\TeamMember;
